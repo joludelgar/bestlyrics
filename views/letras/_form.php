b643697af2003cbaa7e4bce6c30a3a3b84f4da5a
Jose Luis Delgado <joludelgar@gmail.com>
2017-04-14 20:27:32 +0200

    Añadida las normas básicas para las letras

--- views/letras/_form.php
+++ views/letras/_form.php
@@ -8,20 +8,46 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="letra-form">
+<div class="row">
+  <div class="col-xs-6 col-md-8">
+    <div class="letra-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id_idioma')->dropDownList($idiomas,
-        ['prompt' => 'Selecciona el idioma...']
-    ); ?>
+        <?= $form->field($model, 'id_idioma')->dropDownList($idiomas,
+            ['prompt' => 'Selecciona el idioma...']
+        ); ?>
 
-    <?= $form->field($model, 'letra')->textarea(['rows' => '20', 'maxlength' => true]) ?>
+        <?= $form->field($model, 'letra')->textarea(['rows' => '25', 'maxlength' => true]) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
     </div>
-
-    <?php ActiveForm::end(); ?>
-
+  </div>
+  <div class="col-xs-6 col-md-4">
+      <h2>Reglas de escritura</h2>
+      <hr>
+      <ol>
+          <li>Comentarios como "intro", "verso", "puente", "coro", "outro", "hablado", etc... deben ser reemplazados por una fila vacía.</li>
+          <li>Comentarios como "repetir 2 veces", "repetir coro o verso", etc... deben ser eliminados y la parte de la letra involucrada debe repetirse en consecuencia.</li>
+          <li>Todas las partes no habladas como "Ohh", "Ahaaha", "NaNaNa", etc... deben ser mostrados sólo si es parte esencial de la letra.</li>
+          <li>Los nombres de los intérpretes dentro de las letras deben ser evitados / eliminados, incluso cuando se indica una parte específica cantada por el artista.</li>
+      </ol>
+      <hr>
+      <h3>Estilo de escritura</h3>
+      <ol>
+          <li>Por favor recuerde cuidar de corregir caracteres / palabras acentuadas no corregidas.</li>
+          <li>Los nombres propios (de personas, lugares, o ciertas cosas especiales) deben ser mayúsculas.</li>
+      </ol>
+      <hr>
+      <h3>Reglas de puntuación</h3>
+      <ol>
+          <li>No utilice *** o ===, ---</li>
+          <li>Tenga cuidado de las reglas básicas de puntuación.</li>
+      </ol>
+  </div>
 </div>
