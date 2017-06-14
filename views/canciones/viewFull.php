bc6ef33459756cb79069565ce77e0beda26025f7
Jose Luis Delgado <joludelgar@gmail.com>
2017-06-14 13:59:00 +0200

    Pequeño arreglo en vista con video

--- views/canciones/viewFull.php
+++ views/canciones/viewFull.php
@@ -11,17 +11,7 @@ use app\models\Favorito;
 
 $this->registerJsFile('@web/js/yt.js');
 $this->registerCssFile('@web/css/full.css', ['depends' => [\yii\bootstrap\BootstrapAsset::className()]]);
-$js = <<<JS
-window.onload=change_song(substr($model->video, strpos($model->video, '=')+1, 11));
 
-function change_song(videoName) {
-  var videoName;
-
-document.getElementById('overflowContainer').innerHTML = '<iframe id="videoBackground"  src="//www.youtube.com/embed/'+videoName+'?feature=player_embedded&autoplay=1&controls=0&loop=1&modestbranding=1&rel=0&showinfo=0&wmode=transparent&disablekb=1&enablejsapi=1&iv_load_policy=3&origin=http://codepen.io/AcidWolf/pen/HxJvc&playlist='+videoName+'" frameborder="0" allowfullscreen="false"></iframe>';
-}
-JS;
-
-$this->registerJs($js)
 ?>
 <div class="volver">
     <?= Html::a('',['/canciones/view', 'id' => $model->id],[
@@ -38,6 +28,7 @@ $this->registerJs($js)
     <div class="bodyContent" id="scroll">
         <br><br><br><br><br><br><br><br><br><br><br><br><br>
         <?= $model->letras == null ? '' : nl2br($model->letraOriginal->letra) ?>
+        <br><br><br><br><br><br>
     </div>
 
 
